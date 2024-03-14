bf8c8cfc8b1b90898fd0aa98ac1f339dc4103221
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-03-14 17:22:49 +0530

    JWT token signup

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -15,25 +15,35 @@ export const signup = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ error: "Username already exists" });
     }
+    // HASH PASSWORD HERE
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
       fullName,
       username, // Ensure that username is properly set here
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
-
     await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+    if (newUser) {
+      // Here create JWT token
+      generateTokenAndSetCookie(newUser._id, res);
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "invalid user data" });
+    }
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
