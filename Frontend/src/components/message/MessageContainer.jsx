703909a857e1e1fbe7debb573d59e012fbc8832c
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-03-23 11:28:11 +0530

    NoChatsSelected page added

--- Frontend/src/components/message/MessageContainer.jsx
+++ Frontend/src/components/message/MessageContainer.jsx
@@ -4,7 +4,9 @@ import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
   const nochatsselected = true;
-  return (
+  return nochatsselected ? (
+    <NoChatSelected />
+  ) : (
     <div className="md:min-w-[450px] flex flex-col">
       <>
         {/* Header */}
@@ -22,11 +24,10 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
-  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 {authUser.fullName} ❄</p>
+        <p>Welcome 👋 Name❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
